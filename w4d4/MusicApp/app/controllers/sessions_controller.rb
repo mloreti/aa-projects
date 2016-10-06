fe0b732abb403c10b0c65cabb86f61fa91469180
Mickey Loreti <mloreti10@gmail.com>
2016-10-06 15:02:05 -0700

    Allow sign in and sign out

---
+++ w4d4/MusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(session_params[:email], session_params[:password])
+    if @user
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = ["We could not log you in"]
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    logout(current_user)
+    redirect_to new_session_url
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end

77c14be6a3b74e7d0105d752c202320ed6b6cd91
Mickey Loreti <mloreti10@gmail.com>
2016-10-06 19:45:43 -0700

    Finish phase 1

--- w4d4/MusicApp/app/controllers/albums_controller.rb
+++ w4d4/MusicApp/app/controllers/albums_controller.rb
@@ -1,19 +1,35 @@
 class AlbumsController < ApplicationController
   def new
+    @band = Band.find(params[:band_id].to_i)
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      flash[:errors] = @album.errors.full_messages
+      redirect_to new_band_url
+    end
   end
 
   def edit
   end
 
   def show
+    @album = Album.find(params[:id])
+    @band = @album.band
   end
 
   def destroy
   end
 
   def update
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:band_id, :title, :studio)
   end
 end

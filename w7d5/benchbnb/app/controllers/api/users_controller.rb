e3b40ee26ccc59cad0a9e3f89186da57c7288ef8
Mickey Loreti <mloreti10@gmail.com>
2016-10-28 16:26:22 -0700

    Finish backend auth

--- w7d5/benchbnb/app/controllers/api/users_controller.rb
+++ w7d5/benchbnb/app/controllers/api/users_controller.rb
@@ -1,11 +1,20 @@
 class Api::UsersController < ApplicationController
-  def create
-    
-  end
 
-  private
+	def create
+		@user = User.new(user_params)
 
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
+		if @user.save
+			login(@user)
+			render "api/users/show"
+		else
+			render json: @user.errors.full_messages, status: 422
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
+
 end
--- w7d5/benchbnb/config/routes.rb
+++ w7d5/benchbnb/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/create'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
   end
 
   root 'static_pages#root'

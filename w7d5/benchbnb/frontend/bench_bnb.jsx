e4dc8565e95decfea513a424a2113d805847909e
Mickey Loreti <mloreti10@gmail.com>
2016-10-29 15:11:49 -0700

    End of day 1

--- w7d5/benchbnb/frontend/bench_bnb.jsx
+++ w7d5/benchbnb/frontend/bench_bnb.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import {signup} from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>BenchBnB</h1>, root);
+    window.store = store;
+    window.signup = signup;
 });

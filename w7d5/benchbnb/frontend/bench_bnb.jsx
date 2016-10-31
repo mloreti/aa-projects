38cc81aa5d259703787d61152f894cfaec721a18
Mickey Loreti <mloreti10@gmail.com>
2016-10-31 11:27:55 -0700

    Finish Phase 1 benchbnb

--- w7d5/benchbnb/frontend/bench_bnb.jsx
+++ w7d5/benchbnb/frontend/bench_bnb.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+    window.store = store;
 });
